5e08f6b80f75cc361fa90572963d750b076b950e
Nikola Bukovac <nikola.bukovac@outlook.com>
2017-01-16 18:11:39 +0100

    added interfaces

---
+++ NewsPortal/src/NewsPortal/Interfaces/IArticleRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using NewsPortal.Models;
+
+namespace NewsPortal.Interfaces
+{
+    public interface IArticleRepository
+    {
+        Task<List<Article>> GetAll();
+
+        Task<Article> GetById(Guid articleId);
+
+        Task<List<Comment>> GetArticleComments(Guid articleId);
+
+        Task<List<Article>> GetAllWhere(Expression<Func<Article, bool>> predicate);
+
+        void Insert(Article entity);
+
+        void Update(Article entity);
+
+        void Delete(Guid articleId);
+    }
+}
---
+++ NewsPortal/src/NewsPortal/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using NewsPortal.Models;
+
+namespace NewsPortal.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetAll();
+
+        Task<Category> GetById(Guid categoryId);
+
+        Task<List<Article>> GetCategoryArticles(Guid categoryId);
+
+        Task<List<Category>> GetAllWhere(Expression<Func<Category, bool>> predicate);
+
+        void Insert(Category entity);
+
+        void Update(Category entity);
+
+        void Delete(Guid categoryId);
+    }
+}
